158343db0d2225c04e22c373a2bf9bf76e5c5da4
perfalcon <balaveer@gmail.com>
2017-10-13 21:54:33 -0400

    updated

--- IK_server.py
+++ IK_server.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2017 Electric Movement Inc.
-#
-# This file is part of Robotic Arm: Pick and Place project for Udacity
-# Robotics nano-degree program
-#
-# All Rights Reserved.
+### with this it has dropped in the bin.
 
-# Author: Harsh Pandya
 
 # import modules
 import rospy
@@ -39,6 +33,12 @@ def rot_z(alpha):
 
         return R_z
 
+def TF_Matrix(alpha,a,d,q):
+	TF = Matrix([[ cos(q), -sin(q),0,a],
+	     [sin(q)*cos(alpha),cos(q)*cos(alpha),-sin(alpha),-sin(alpha)*d],
+	     [sin(q)*sin(alpha),cos(q)*sin(alpha),cos(alpha),cos(alpha)*d],
+	     [0,0,0,1]])
+	return TF
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -73,53 +73,24 @@ def handle_calculate_IK(req):
 	# Create individual transformation matrices
 	#
 	#
-	T0_1 = Matrix([[ cos(q1), -sin(q1),0,a0],
-                        [sin(q1)*cos(alpha0),cos(q1)*cos(alpha0),-sin(alpha0),-sin(alpha0)*d1],
-                        [sin(q1)*sin(alpha0),cos(q1)*sin(alpha0),cos(alpha0),cos(alpha0)*d1],
-                        [0,0,0,1]])
-        T0_1=T0_1.subs(s)
-        T1_2 = Matrix([[ cos(q2), -sin(q2),0,a0],
-                        [sin(q2)*cos(alpha1),cos(q2)*cos(alpha1),-sin(alpha1),-sin(alpha1)*d2],
-                        [sin(q2)*sin(alpha1),cos(q2)*sin(alpha1),cos(alpha1),cos(alpha1)*d2],
-                        [0,0,0,1]])
-        T1_2=T1_2.subs(s)
-        T2_3 = Matrix([[ cos(q3), -sin(q3),0,a0],
-                                                                [sin(q3)*cos(alpha2),cos(q3)*cos(alpha2),-sin(alpha2),-sin(alpha2)*d3],
-                                                                [sin(q3)*sin(alpha2),cos(q3)*sin(alpha2),cos(alpha2),cos(alpha2)*d3],
-                                                                [0,0,0,1]])
-        T2_3=T2_3.subs(s)
-        T3_4 = Matrix([[ cos(q4), -sin(q4),0,a0],
-                                                                [sin(q4)*cos(alpha3),cos(q4)*cos(alpha3),-sin(alpha3),-sin(alpha3)*d4],
-                                                                [sin(q4)*sin(alpha3),cos(q4)*sin(alpha3),cos(alpha3),cos(alpha3)*d4],
-                                                                [0,0,0,1]])
-        T3_4=T3_4.subs(s)
-        T4_5 = Matrix([[ cos(q5), -sin(q5),0,a0],
-                                                                [sin(q5)*cos(alpha4),cos(q5)*cos(alpha4),-sin(alpha4),-sin(alpha4)*d5],
-                                                                [sin(q5)*sin(alpha4),cos(q5)*sin(alpha4),cos(alpha4),cos(alpha4)*d5],
-                                                                [0,0,0,1]])
-        T4_5=T4_5.subs(s)
-        T5_6 = Matrix([[ cos(q6), -sin(q6),0,a0],
-                        [sin(q6)*cos(alpha5),cos(q6)*cos(alpha5),-sin(alpha5),-sin(alpha5)*d6],
-                                                                [sin(q6)*sin(alpha5),cos(q6)*sin(alpha5),cos(alpha5),cos(alpha5)*d6],
-                                                                [0,0,0,1]])
-        T5_6=T5_6.subs(s)
-        T6_G = Matrix([[ cos(q7), -sin(q7),0,a0],
-                                                                [sin(q7)*cos(alpha6),cos(q7)*cos(alpha6),-sin(alpha6),-sin(alpha6)*d7],
-                                                                [sin(q7)*sin(alpha6),cos(q7)*sin(alpha6),cos(alpha6),cos(alpha6)*d7],
-                                                                [0,0,0,1]])
-        T6_G=T6_G.subs(s)
 
-	
-	#T0_2=simplify(T0_1*T1_2)
-        #T0_3=simplify(T0_2*T2_3)
-        #T0_4=simplify(T0_3*T3_4)
-        #T0_5=simplify(T0_4*T4_5)
-        #T0_6=simplify(T0_5*T5_6)
-        #T0_G=simplify(T0_6*T6_G)
 
-	# Extract rotation matrices from the transformation matrices
-	
-	    R_z = rot_z(radians(180))
+	##Homogenous Transforms		
+    	T0_1 = TF_Matrix(alpha0,a0,d1,q1).subs(s)
+    	T1_2 = TF_Matrix(alpha1,a1,d2,q2).subs(s)
+    	T2_3 = TF_Matrix(alpha2,a2,d3,q3).subs(s)
+        T3_4 = TF_Matrix(alpha3,a3,d4,q4).subs(s)
+        T4_5 = TF_Matrix(alpha4,a4,d5,q5).subs(s)
+	T5_6 = TF_Matrix(alpha5,a5,d6,q6).subs(s)
+	T6_EE = TF_Matrix(alpha6,a6,d7,q7).subs(s)
+	T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+
+	#Transformation from Base to Joint 3.
+        R0_3 = simplify(T0_1 * T1_2 * T2_3)	
+
+	#correction needed to account of orientation difference between definition of 
+				#Gripper_link in URDF versus DH Convention
+       	R_z = rot_z(radians(180))
         R_y = rot_y(radians(-90))
 
         R_corr= simplify(R_z * R_y)
@@ -142,42 +113,61 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
-        
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	        
-            r_roll = rot_x(roll)
+            ### Your IK code here 
+	    
+	    ###
+	    # Calculate joint angles using Geometric IK method
+	      #R0_6 = Rrpy * R_corr
+
+ 	    r_roll = rot_x(roll)
             r_pitch = rot_y(pitch)
             r_yaw = rot_z(yaw)
-            R0_6= r_roll * r_pitch * r_yaw * R_corr
-        #Calculate WC:
-        
-			p_ee = Matrix([px,py,pz])
+            R0_6= r_roll * r_pitch * r_yaw * R_corr  # Compensate for rotation discrepancy between DH parameters and Gazebo
+           
+	          
+           
+	    #Calculate Wrist Center:(WC)
+            p_ee = Matrix([px,py,pz])
             WC = p_ee - R0_6 * Matrix([0, 0, s[d7]])
+	    pprint(WC)
 
-		# Calculate joint angles using Geometric IK method	
-			
-            theta1 = atan2(WC[1],WC[0]) # @2 = atan2(y,x)
+            theta1 = (atan2(WC[1],WC[0])).evalf() # @2 = atan2(y,x)
 	    
-	        s_a = 1.501
+	    s_a = 1.501
             s_b = sqrt(pow((sqrt(WC[0]*WC[0]+WC[1]*WC[1])-0.35),2)+pow((WC[2]-0.75),2))
             s_c = 1.25
 
-	        angle_a = acos((s_c**2 + s_b**2 - s_a**2)/2*s_b*s_c)
-            angle_b = acos((s_c**2 + s_a**2 - s_b**2)/2*s_c*s_a)
-            angle_c = acos((s_a**2 + s_b**2 - s_c**2)/2*s_a*s_b)
+	    angle_a = (acos((s_c**2 + s_b**2 - s_a**2)/(2*s_b*s_c))).evalf()
+            angle_b = (acos((s_c**2 + s_a**2 - s_b**2)/(2*s_c*s_a))).evalf()
+            angle_c = (acos((s_a**2 + s_b**2 - s_c**2)/(2*s_a*s_b))).evalf()
 
-            theta2 = pi/2 - angle_a - atan2(WC[2]-s[d1],sqrt(WC[0]**2 + WC[1]**2 - s[a1]))
-            theta3 = pi/2 - (angle_b + 0.036)
+            theta2 = (pi/2 - angle_a - atan2(WC[2]-s[d1],sqrt(WC[0]**2 + WC[1]**2 - s[a1]))).evalf()
+            theta3 = (pi/2 - (angle_b + 0.036)).evalf()
 
-            R0_3 = T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
+          
+           
             R0_3 = R0_3.evalf(subs={q1:theta1,q2:theta2,q3:theta3})
-            R3_6 = R0_3.inv("LU")*R0_6
+            R0_3 = R0_3[0:3, 0:3]
+	    pprint(R0_3)
 
-            theta4 = atan2(R3_6[2,2],-R3_6[0,2])
-            theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2),R3_6[1,2])
-            theta6 = atan2(R3_6[1,1],R3_6[1,0])
+	    #Euler Angles -- Inverse Kinematics lesson
+            #R3_6 = R0_3.inv("LU")*R0_6
+	    R3_6 = R0_3.T * R0_6
+
+
+	    # Theta 4,5 & 6 angles.
+
+            theta4 = (atan2(R3_6[2,2],-R3_6[0,2])).evalf()
+            theta5 = (atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2),R3_6[1,2])).evalf()
+            theta6 = (atan2(R3_6[1,1],R3_6[1,0])).evalf()
 	
-            print "theta1, theta2, theta3, theta4, theta5, theta6",theta1,theta2,theta3,theta3,theta4,theta5,theta6 				
+           
+            print('theta1: ' + str(theta1))
+	    print('theta2: ' + str(theta2))
+	    print('theta3: ' + str(theta3))
+	    print('theta4: ' + str(theta4))
+	    print('theta5: ' + str(theta5))
+	    print('theta6: ' + str(theta6))	
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
